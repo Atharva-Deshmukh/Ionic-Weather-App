43147cd111c082bc4be5a7bc6b9b2876c3425df7
Atharva <deshmukhatharva2103@gmail.com>
2023-04-22 12:51:42 +0530

    Login SignUp pages -> ICONS added

--- weather-app/src/app/app.module.ts
+++ weather-app/src/app/app.module.ts
@@ -5,12 +5,28 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  declarations:
+  [
+    AppComponent,
+    LoginPageComponent,
+    SignupPageComponent
+  ],
+  imports:
+  [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule],
+  providers:
+  [
+    {
+      provide: RouteReuseStrategy, useClass: IonicRouteStrategy
+    }
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
